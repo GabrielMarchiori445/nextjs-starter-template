82df12441367a778a85f679d1918b04fefa00458
GabrielMarchiori445 <kelvingabriel8877@gmail.com>
2025-06-11 01:41:32 -0300

    Add .next/static/chunks/src_app_page_tsx_a9068ab0._.js 1749616891395

---
+++ .next/static/chunks/src_app_page_tsx_a9068ab0._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_15b0f4fc._.js",
+  "static/chunks/src_c764db68._.js",
+  "static/chunks/src_components_ConceptMapper_concept-mapper_ed9b5e72.css"
+],
+    source: "dynamic"
+});
